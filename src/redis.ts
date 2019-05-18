c022a684e5c768a0d06f94d82c8bd6ee822caf94
Ian Likono <ianlikono@gmail.com>
2019-05-18 13:56:35 -0500

    pdated redis

--- src/redis.ts
+++ src/redis.ts
@@ -2,5 +2,5 @@ import * as Redis from "ioredis";
 
 export const redis =
   process.env.NODE_ENV === "production"
-    ? new Redis('agoraexpo-redis.zfbnx0.0001.use1.cache.amazonaws.com:6379')
+    ? new Redis("agoraexpo-redis.zfbnx0.0001.use1.cache.amazonaws.com ")
     : new Redis();
